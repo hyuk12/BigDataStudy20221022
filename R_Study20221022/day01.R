2f17c96503299e123fc2180c1f50bf9416b64bf9
= <>
2022-10-22 17:00:45 +0900

    2022.10.22 bigdata study day01

---
+++ R_Study20221022/day01.R
@@ -0,0 +1,12 @@
+x = c(1,4)
+y = c(5,5)
+plot(x,y)
+z = 15
+
+# ctrl + shift + o = 파일불러오기
+# ctrl + shift + n = 새로만들기
+# ctrl + s = 파일저장하기
+# alt + shift + k = 모든 단축키 확인
+# ctrl + shift + c = 주석처리리
+
+
